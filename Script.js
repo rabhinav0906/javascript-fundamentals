744cc71e1ded4e366e2c81c73b6af4b8b18ba536
Abhinav Rai <rabhinav0906@gmail.com>
2026-01-30 19:17:13 +0530

    Loop Added

--- Script.js
+++ Script.js
@@ -107,3 +107,18 @@ const person = {
 
 console.log(person.name);
 console.log(person["name"]);
+
+//for loop
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+//while loop
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
